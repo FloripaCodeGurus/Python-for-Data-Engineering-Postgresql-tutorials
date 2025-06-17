f532c0527f8454772bf481d0e17fc2c2369689a2
agilproducts <agilfiles2023@gmail.com>
2025-06-17 18:23:18 -0300

    Editing create_table

--- tutorial_2/create_tables_2.py
+++ tutorial_2/create_tables_2.py
@@ -8,15 +8,13 @@ def create_tables():
         if conn:
             cur = conn.cursor()
             cur.execute("""
-                
             CREATE TABLE IF NOT EXISTS people (
                 id SERIAL PRIMARY KEY,
                 first_name VARCHAR(50),
                 last_name VARCHAR(50),
                 age INTEGER,
                 city_id INTEGER
-            );
-                
+            );  
             CREATE TABLE IF NOT EXISTS people_for_union (
                 id SERIAL PRIMARY KEY,
                 first_name VARCHAR(50),
@@ -24,35 +22,27 @@ def create_tables():
                 age INTEGER,
                 city_id INTEGER
             );
-            
-                
            CREATE TABLE IF NOT EXISTS countries (
             id SERIAL PRIMARY KEY,
             name TEXT NOT NULL
             );
-
-                
            CREATE TABLE IF NOT EXISTS states (
             id SERIAL PRIMARY KEY,
             name TEXT NOT NULL,
             uf CHAR(2) NOT NULL,
             country_id INT NOT NULL REFERENCES countries(id)
             );
-                
             CREATE TABLE IF NOT EXISTS cities (
             id SERIAL PRIMARY KEY,
             city_name TEXT NOT NULL,
             state_id INT NOT NULL REFERENCES states(id)
             );
-
-
             CREATE TABLE IF NOT EXISTS addresses (
             id SERIAL PRIMARY KEY,
             street TEXT NOT NULL,
             number TEXT NOT NULL,
             city_id INT NOT NULL REFERENCES cities(id)
         );
-
         """)
             cur.close()
             conn.commit()
